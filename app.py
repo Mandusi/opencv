9ff31598d8643f2ffbbf00300a56400d88a0bb44
Mandusi <osman.ekinci@mirket.dev>
2024-11-03 19:37:55 +0300

    functions

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 import cv2 as cv 
 
+
+# Read Image
 img = cv.imread('photos/f16.jpg')
 
-img_resized = cv.resize(img, (800,600))
+# # Resize Image (changes resolution)
+# img_resized = cv.resize(img, (800,600))
 
-cv.rectangle(img_resized, (20,30), (700,540), thickness=2, color=(200,200,12))
+# # Draw a Rectangle to an Image
+# cv.rectangle(img_resized, (20,30), (700,540), thickness=2, color=(200,200,12))
+# cv.imshow('F-4', img_resized)
 
-cv.imshow('F-4', img_resized)
+# # Gray Scale 
+# img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow("Grayscale", img_gray)
+
+# Blur
+img_blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow("Blur", img_blur)
+
+# Edge 
+img_edge = cv.Canny(img_blur, 125, 175)
+cv.imshow("Edges",img_edge)
+
+
 
 cv.waitKey(0)
+
+
 
